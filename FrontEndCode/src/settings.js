d595644de3407ee5e3dd6b44f361855f77d2159b
kjk_mr <goodMornning_kjk@163kkx.com>
2022-05-06 22:10:16 +0800

    初始化vue代码

---
+++ FrontEndCode/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '阿K学尚融宝前端',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
